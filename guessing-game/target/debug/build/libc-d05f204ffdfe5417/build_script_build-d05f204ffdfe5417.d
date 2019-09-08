a77da0a66b91377ea0e8c6859964e3cca0f4b2b1
Thvarzil <louisblebohec@gmail.com>
2019-09-08 13:20:18 -0600

    Still fixing initialization issues

--- guessing-game/target/debug/build/libc-d05f204ffdfe5417/build_script_build-d05f204ffdfe5417.d
+++
@@ -1,5 +0,0 @@
-/Users/basilica/Desktop/Dev/theRustBucket/guessing-game/target/debug/build/libc-d05f204ffdfe5417/build_script_build-d05f204ffdfe5417: /Users/basilica/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.62/build.rs
-
-/Users/basilica/Desktop/Dev/theRustBucket/guessing-game/target/debug/build/libc-d05f204ffdfe5417/build_script_build-d05f204ffdfe5417.d: /Users/basilica/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.62/build.rs
-
-/Users/basilica/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.62/build.rs:
--- guessing-game/target/debug/deps/rand-3fd31a101c8f1238.d
+++
@@ -1,9 +0,0 @@
-/Users/basilica/Desktop/Dev/theRustBucket/guessing-game/target/debug/deps/rand-3fd31a101c8f1238.rmeta: /Users/basilica/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/lib.rs /Users/basilica/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/distributions/mod.rs /Users/basilica/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/rand_impls.rs
-
-/Users/basilica/Desktop/Dev/theRustBucket/guessing-game/target/debug/deps/librand-3fd31a101c8f1238.rlib: /Users/basilica/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/lib.rs /Users/basilica/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/distributions/mod.rs /Users/basilica/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/rand_impls.rs
-
-/Users/basilica/Desktop/Dev/theRustBucket/guessing-game/target/debug/deps/rand-3fd31a101c8f1238.d: /Users/basilica/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/lib.rs /Users/basilica/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/distributions/mod.rs /Users/basilica/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/rand_impls.rs
-
-/Users/basilica/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/lib.rs:
-/Users/basilica/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/distributions/mod.rs:
-/Users/basilica/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/rand_impls.rs:
